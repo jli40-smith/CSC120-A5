605c85b83bd9ccdbf44b7dabada0c9e028503acf
JOANNA LI <jli40@smith.edu>
2022-10-18 17:23:49 -0400

    Shirt.java implemented and prints correctly

--- Shirt.java
+++ Shirt.java
@@ -7,10 +7,17 @@
 public class Shirt {
 
     /*
-     * TODO: Modify this method to print ASCII Art Shirt
+     * Prints ASCII art depicting a shirt to the console
      */
     public void display() {
-        System.out.println("Shirt?);
+        System.out.println("        __,--./|.--,__");
+        System.out.println("      .`  \\ \\    / /  `.");
+        System.out.println("    .`     \\ |  | /     `."); 
+        System.out.println("   /   /\\ --- |/ --- /\\   \\"); 
+        System.out.println("  /   / | \\_/ |0 \\_/ | \\   \\"); 
+        System.out.println(" /===/  \\     |      /  \\===\\"); 
+        System.out.println("/___/   /     |0     \\   \\___\\"); 
+        System.out.println("        |_____/\\_____|");  
     }
 
     /* main method (for testing) */
