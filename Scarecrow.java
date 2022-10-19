002364fdd74cdd4481ee8a5edaff7fd80aa76dea
JOANNA LI <jli40@smith.edu>
2022-10-19 10:26:57 -0400

    When no command line arguments are passed in, banner displays the default message correctly

--- Scarecrow.java
+++ Scarecrow.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /*
  * Scarecrow class (template)
  * Assignment 5: Bringing it All Together
@@ -36,14 +38,25 @@ class Scarecrow {
 
     /* Main method (for testing) */
     public static void main(String[] args) {
+        ArrayList<String> userInput = new ArrayList<String>(); 
+        String message; 
+        
+        if (args.length > 0) {
+            
+            for (int i = 0; i < args.length; i++) {
+                userInput.add(args[i]); 
+            }
+            message = String.join(" ", userInput); 
 
-        Scarecrow myScarecrow = new Scarecrow(new Pumpkin(), new Shirt(), new Pants(), new Banner("Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus Amogus Sus "));
+        } else {
+            message = "Happy Halloween!"; 
+        }
+
+        Scarecrow myScarecrow = new Scarecrow(new Pumpkin(), new Shirt(), new Pants(), new Banner(message));
 
         // If a message was passed in on the command line, extract and store it
         // TODO: in Step 4, you'll pass this value along to your Banner constructor
-        //if (args.length > 0) {
-            //myScarecrow.message = args[0];
-        //}
+        
 
         myScarecrow.display();
     }
